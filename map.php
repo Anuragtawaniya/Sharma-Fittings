ad8651c1151577da03d06e329fc4aaf6791fbdfa
anuragtawaniya <53399643+Anuragtawaniya@users.noreply.github.com>
2024-07-04 20:04:17 +0530

    Add files via upload

---
+++ map.php
@@ -0,0 +1,6 @@
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.4181301770404!2d88.35307377433654!3d22.563459633307055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0277077a24b011%3A0x86fb4fe6812fe774!2sSharma%20Aluminum%20Fittings!5e0!3m2!1sen!2sin!4v1718557029884!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <!-- kolkata location sjnm map -->
+
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.5340548829063!2d77.20928547455773!3d28.64372378357412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd42472436e9%3A0x9528b5cd7c99e291!2sSaraswat%20Hardware!5e0!3m2!1sen!2sin!4v1718557171333!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
